e39b894267652a576cdeb0ae5acf1c04446c372b
Nina <pinkangelssugar@gmail.com>
2020-08-20 12:46:24 +0300

    Updates

--- precache-manifest.1b59ebc5cbe8cca82bb17db269ca17e4.js
+++ precache-manifest.1b59ebc5cbe8cca82bb17db269ca17e4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "79fdb0ea0860f41e1e2e27ff6cbd0281",
+    "revision": "9c905664592019d1e3e771823e12ffd6",
     "url": "/announcement/index.html"
   },
   {
-    "revision": "b6aa66d6a12e22ccfd7e",
+    "revision": "4ff6335625f94b539221",
     "url": "/announcement/static/css/main.4e1ec80a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/announcement/static/js/2.bb260d21.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b6aa66d6a12e22ccfd7e",
-    "url": "/announcement/static/js/main.76fd2ff3.chunk.js"
+    "revision": "4ff6335625f94b539221",
+    "url": "/announcement/static/js/main.e7998488.chunk.js"
   },
   {
     "revision": "93cae4b2addee053f7bb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/announcement/precache-manifest.8bc67734c4b84ed4b31ec8cf7548d321.js"
+  "/announcement/precache-manifest.1b59ebc5cbe8cca82bb17db269ca17e4.js"
 );
 
 self.addEventListener('message', (event) => {
