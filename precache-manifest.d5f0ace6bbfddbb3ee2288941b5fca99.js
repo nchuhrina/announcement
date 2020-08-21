cd41c20243d4b0182347521d078266e0637e90ed
Nina <pinkangelssugar@gmail.com>
2020-08-21 11:43:30 +0300

    Updates

--- precache-manifest.d5f0ace6bbfddbb3ee2288941b5fca99.js
+++ precache-manifest.d5f0ace6bbfddbb3ee2288941b5fca99.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a2de4df96b2464f66fc2ad148b005297",
+    "revision": "62a4ec83caee0654e48d458f415bcf68",
     "url": "/announcement/index.html"
   },
   {
-    "revision": "8bd679a0fa0399aea446",
+    "revision": "867ad0dc423cab07048f",
     "url": "/announcement/static/css/main.4e1ec80a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/announcement/static/js/2.de9acf3c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8bd679a0fa0399aea446",
-    "url": "/announcement/static/js/main.865cf2e6.chunk.js"
+    "revision": "867ad0dc423cab07048f",
+    "url": "/announcement/static/js/main.f085b2bb.chunk.js"
   },
   {
     "revision": "93cae4b2addee053f7bb",
